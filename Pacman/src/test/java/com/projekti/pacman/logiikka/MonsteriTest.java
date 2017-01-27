73cd9cea740b29c41b1d0464df69e4e023bb4a67
Jimi <jimi8.lang@gmail.com>
2017-01-27 21:40:15 +0200

    testi monsteri luokan sunnan arvojalle

--- Pacman/src/test/java/com/projekti/pacman/logiikka/MonsteriTest.java
+++ Pacman/src/test/java/com/projekti/pacman/logiikka/MonsteriTest.java
@@ -5,10 +5,40 @@
  */
 package com.projekti.pacman.logiikka;
 
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  *
  * @author langjimi
  */
 public class MonsteriTest {
+    
+    Monsteri monsteri;
+    
+    @Before
+    public void setUp(){
+        monsteri = new Monsteri(0,0);
+    }
+    
+    @Test
+    public void arvoSuuntaAsettaaUudenSuunnan() {
+        
+        monsteri.arvoSuunta();
+        
+        int y = 0;
+        
+        String x = monsteri.getSuunta() + "";
+        
+        if(x.equals("STOP")){
+            y=1;
+        }
+        
+        System.out.println(x);
+        
+        assertEquals(0, y);
+    }
     
 }
