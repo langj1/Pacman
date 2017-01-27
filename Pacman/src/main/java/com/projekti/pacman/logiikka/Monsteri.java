792ad9b42e30ed00b41ecec82f8854f218335145
Jimi <jimi8.lang@gmail.com>
2017-01-27 18:39:23 +0200

    monsteria aloitettu

--- Pacman/src/main/java/com/projekti/pacman/logiikka/Monsteri.java
+++ Pacman/src/main/java/com/projekti/pacman/logiikka/Monsteri.java
@@ -5,10 +5,41 @@
  */
 package com.projekti.pacman.logiikka;
 
+import com.projekti.pacman.Suunta;
+
 /**
  *
  * @author langjimi
  */
 public class Monsteri {
+    
+    private int xKoordinaatti;
+    private int yKoordinaatti;
+    private Suunta suunta;
+    
+    public Monsteri(int alkuX, int alkuY){
+        
+        this.xKoordinaatti = alkuX;
+        this.yKoordinaatti = alkuY;
+        this.suunta = suunta.STOP;
+        
+    }
+
+    public Suunta getSuunta() {
+        return suunta;
+    }
+
+    public int getxKoordinaatti() {
+        return xKoordinaatti;
+    }
+
+    public int getyKoordinaatti() {
+        return yKoordinaatti;
+    }
+
+    public void liiku(){
+        
+    }
+    
     
 }
--- Pacman/src/main/java/com/projekti/pacman/logiikka/Pacman.java
+++ Pacman/src/main/java/com/projekti/pacman/logiikka/Pacman.java
@@ -7,19 +7,50 @@ package com.projekti.pacman.logiikka;
 
 import com.projekti.pacman.Suunta;
 
-
-
-/**
- *
- * @author langjimi
- */
 public class Pacman {
-    
+
     private Suunta suunta;
-    
-    public Pacman(){
-        
-        this.suunta = STOP;
+    private int xKordinaatti;
+    private int yKordinaatti;
+
+    public Pacman(int alkuX, int alkuY) {
+
+        this.suunta = suunta.STOP;
+        this.xKordinaatti = alkuX;
+        this.yKordinaatti = alkuY;
+
     }
-    
+
+    public Suunta getSuunta() {
+        return suunta;
+    }
+
+    public void setSuunta(Suunta suunta) {
+        this.suunta = suunta;
+    }
+
+    public int getxKordinaatti() {
+        return xKordinaatti;
+    }
+
+    public int getyKordinaatti() {
+        return yKordinaatti;
+    }
+
+    public void liiku() {
+
+        if (suunta == suunta.VASEN) {
+
+        }
+        if (suunta == suunta.OIKEA) {
+
+        }
+        if (suunta == suunta.YLOS) {
+
+        }
+        if (suunta == suunta.ALAS) {
+
+        }
+    }
+
 }
