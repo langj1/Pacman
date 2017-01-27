f1a483ef9aec2299be3f46fb4731533c47a83969
Jimi <jimi8.lang@gmail.com>
2017-01-27 21:31:04 +0200

    Luokan Liikkuva metodeille testit

---
+++ Pacman/src/test/java/com/projekti/pacman/logiikka/LiikkuvaTest.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.projekti.pacman.logiikka;
+
+import com.projekti.pacman.Suunta;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author langjimi
+ */
+public class LiikkuvaTest {
+
+    Liikkuva liikkuva;
+
+    @Before
+    public void setUp() {
+        liikkuva = new Liikkuva(12, 324);
+    }
+
+    @Test
+    public void konstruktoriAsettaaOikeanXArvon() {;
+
+        assertEquals(12, liikkuva.getxKordinaatti());
+    }
+
+    @Test
+    public void konstruktoriAsettaaOikeanYArvon() {
+
+        assertEquals(324, liikkuva.getyKordinaatti());
+    }
+
+    @Test
+    public void liikuToimiiJosSuuntaOikea() {
+
+        liikkuva.setSuunta(Suunta.OIKEA);
+        liikkuva.liiku();
+
+        assertEquals(13, liikkuva.getxKordinaatti());
+    }
+
+    @Test
+    public void liikuToimiiJosSuuntaVasen() {
+
+        liikkuva.setSuunta(Suunta.VASEN);
+        liikkuva.liiku();
+
+        assertEquals(11, liikkuva.getxKordinaatti());
+    }
+
+    @Test
+    public void liikuToimiiJosSuuntaYlos() {
+
+        liikkuva.setSuunta(Suunta.YLOS);
+        liikkuva.liiku();
+
+        assertEquals(323, liikkuva.getyKordinaatti());
+    }
+
+    @Test
+    public void liikuToimiiJosSuuntaAlas() {
+
+        liikkuva.setSuunta(Suunta.ALAS);
+        liikkuva.liiku();
+
+        assertEquals(325, liikkuva.getyKordinaatti());
+    }
+
+    @Test
+    public void liikuToimiiJosSuuntaStop() {
+
+        liikkuva.setSuunta(Suunta.STOP);
+        liikkuva.liiku();
+
+        assertEquals(324, liikkuva.getyKordinaatti());
+        assertEquals(12, liikkuva.getxKordinaatti());
+    }
+}
