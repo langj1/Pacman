e9182c4fedafa2a6e53f864204b49fe66ae424f7
Jimi <jimi8.lang@gmail.com>
2017-01-27 18:27:34 +0200

    Uusia luokkia lisätty

---
+++ Pacman/src/main/java/com/projekti/pacman/logiikka/Pacman.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.projekti.pacman.logiikka;
+
+import com.projekti.pacman.Suunta;
+
+
+
+/**
+ *
+ * @author langjimi
+ */
+public class Pacman {
+    
+    private Suunta suunta;
+    
+    public Pacman(){
+        
+        this.suunta = STOP;
+    }
+    
+}
