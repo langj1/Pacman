52ef56f3fb6c4fbbe15a43132476fe1d25340b92
Jimi <jimi8.lang@gmail.com>
2017-02-10 15:20:25 +0200

    uusi Piirtoalusta luokka

---
+++ Pacman/src/main/java/com/projekti/pacman/gui/Piirtoalusta.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.projekti.pacman.gui;
+
+import com.projekti.pacman.logiikka.Monsteri;
+import com.projekti.pacman.peli.Kentta;
+import com.projekti.pacman.peli.Peli;
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author langjimi
+ */
+public class Piirtoalusta extends JPanel {
+
+    private Peli peli;
+    private int sivu;
+    
+
+    public Piirtoalusta(Peli peli, int sivu) {
+
+        this.peli = peli;
+        this.sivu = sivu;
+        ;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+        
+        int[][] kentta = peli.getKentta().getKentta();
+
+        for (int i = 0; i < kentta[0].length; i++) {
+            for (int j = 0; j < kentta.length; j++) {
+
+                if(kentta[j][i] == 0){
+                    
+                    g.setColor(Color.BLUE);
+                    
+                    g.fillRect(j, i, sivu, sivu);
+                }
+                
+                if(kentta[j][i] == 2){
+                    
+                    g.setColor(Color.PINK);
+                    
+                    g.fillOval(j, i, sivu/4, sivu/4);
+                }
+                
+                if(kentta[j][i] == 3){
+                    
+                    g.setColor(Color.RED);
+                    
+                    g.fillOval(j, i, sivu, sivu);
+                }
+                
+                if(kentta[j][i] == 4){
+                    
+                    g.setColor(Color.YELLOW);
+                    
+                    g.fillOval(j, i, sivu, sivu);
+                }
+            }
+        }
+
+    }
+    
+    public void paivita(){
+        repaint();
+    }
+}
