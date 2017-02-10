65c54f0fdff77dacb755bea65da64434f15c4d6f
Jimi <jimi8.lang@gmail.com>
2017-02-10 15:00:08 +0200

    uusi Nappaimistonkuuntelija luokka

---
+++ Pacman/src/main/java/com/projekti/pacman/gui/Nappaimistonkuuntelija.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.projekti.pacman.gui;
+
+import com.projekti.pacman.Suunta;
+import com.projekti.pacman.logiikka.Pacman;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/**
+ *
+ * @author langjimi
+ */
+public class Nappaimistonkuuntelija implements KeyListener {
+    
+    private Pacman pacman;
+    
+    public Nappaimistonkuuntelija(Pacman pacman){
+        this.pacman = pacman;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        ; //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        
+        if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+            pacman.setSuunta(Suunta.OIKEA);
+        }
+        
+        if(e.getKeyCode() == KeyEvent.VK_LEFT){
+            pacman.setSuunta(Suunta.VASEN);
+        }
+            
+        if(e.getKeyCode() == KeyEvent.VK_UP){
+            pacman.setSuunta(Suunta.YLOS);
+        }
+        
+        if(e.getKeyCode() == KeyEvent.VK_DOWN){
+            pacman.setSuunta(Suunta.ALAS);
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        ; //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
